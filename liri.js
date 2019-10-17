cd418e24e0742090e2fdd7c4056e890791e65f29
Alfie <53308938+philldesign@users.noreply.github.com>
2019-10-17 15:06:12 -0400

    switch case and arguments initiated

--- liri.js
+++ liri.js
@@ -1,26 +1,83 @@
 require("dotenv").config();
 
+//gets keys from Spotify API
 var keys = require("./keys.js");
-// console.log(keys)
-// var spotify = new Spotify(keys.spotify);  
+
+//import axios call
 const axios = require('axios');
-artist = ''
-//   command #1   * concert-this * (Bands in Town API)
+
+const arg = process.argv;
+
+//input name of artist, movie or song by user :
+entertainment = ''
+
+// –––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+//switch method applied to identify different kind of inputs and call their respective functions:
+switch(arg){
+  //if input is an artists:
+  case "concert-this":
+      concertThis(entertainment);
+  break;
+  //if input is a song:
+  case "spotify-this-song":
+    if(entertainment){
+      spotifyThis(entertainment);
+    } else{
+      spotifyThis("The Sign, Ace of Base");
+    }
+  break;
+  //if input is a movie:
+  case "movie-this":
+    if(entertainment){
+      omdbData(entertainment)
+    } else{
+      omdbData("Mr. Nobody")
+    }
+  break;
+  //if input is a :
+  case "do-what-it-says":
+    doThing();
+  break;
+
+  default:
+    console.log("concert song or movie? Or Help?");
+  break;
+}
+// –––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+//   Command #1   * concert-this * (Bands in Town API)
 //         i.e.   node liri.js concert-this <artist/band name here>
 // };
 // var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 // Celine Dion Example: `https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp`
+
 function concertThis() {
 axios.get(`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`)
     .then((response) => {
-    // Then we print out the imdbRating
+
       console.log(response);
 
     });
   }
   concertThis();
 
+// –––––––––––––––––––––––––––––––––––––––––––––––––––––
+//   Command #2   * spotify-this * (Spotify API)
 //   * `spotify-this-song`
+
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);  
+
+spotify
+ .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+ .then(function(data) {
+   console.log(data); 
+ })
+ .catch(function(err) {
+   console.error('Error occurred: ' + err); 
+ });
+
 function spotifyThis() {
   
 axios.get(`https://api.spotify.com/v1/search?query=tania+bowra&offset=0&limit=20&type=artist`)
@@ -31,7 +88,10 @@ axios.get(`https://api.spotify.com/v1/search?query=tania+bowra&offset=0&limit=20
     });
   }
     spotifyThis();
-    
+
+// –––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+//   Command #3   * movie-this * (OMDB API)
 
 // We then run the request with axios module on a URL with a JSON
 // axios.get('http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy')
@@ -39,18 +99,4 @@ axios.get(`https://api.spotify.com/v1/search?query=tania+bowra&offset=0&limit=20
 //     // Then we print out the imdbRating
 //       console.log(response);
 
- var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify({
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
-});
- 
-spotify
-  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-  .then(function(data) {
-    console.log(data); 
-  })
-  .catch(function(err) {
-    console.error('Error occurred: ' + err); 
-  });
+// –––––––––––––––––––––––––––––––––––––––––––––––––––––
